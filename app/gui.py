051807553fd6eb765ff56919f784e9eaebe760ed
Unf0rgiv3n <jakub.kwia2@wp.pl>
2022-04-08 14:43:41 +0200

    initial gui

---
+++ app/gui.py
@@ -0,0 +1,83 @@
+from ast import Call
+from pydoc import text
+import tkinter as tk
+from tkinter import Place, ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+from turtle import bgcolor
+from typing import Callable
+
+from key_gens import generate_rsa_keys, send_rsa_key
+
+class Gui(tk.Tk):
+    def __init__(self, title: str):
+        super().__init__()
+
+        self.title(title)
+        self.geometry('600x400')
+        self.resizable(False, False)
+        self.style= ttk.Style()
+        self.style.theme_use('clam')
+
+        self._create_grid(row_num=5, col_num=2)
+
+        self._gen_rsa_btn = self._create_button(row=0,column=0, text='Generate RSA keys', command=generate_rsa_keys)
+        self._send_rsa_btn = self._create_button(row=1,column=0, text='Send RSA key', command=send_rsa_key)
+
+        self._select_file_btn = self._create_button(row=1,column=1, text='Select file', command=self.select_file)
+
+        self._text_box = self._create_text_box(row=2, column=0, rowspan=3, columnspan=1)
+        self._send_text_box_btn = self._create_button(row=5,column=0, text='Send text', command=None)
+
+        self.combobox = self._create_combobox(row=0,column=1)
+
+    def _create_grid(self, row_num: int, col_num: int):
+        for i in range(row_num):
+            self.rowconfigure(i, weight=1)
+        
+        for i in range(col_num):
+            self.columnconfigure(i, weight=1)
+
+    def _create_button(self, row: int, column: int, text: str, command: Callable[[], None]) ->  ttk.Button:
+        button = ttk.Button(self, text=text, command=command)
+        button.grid(row=row,column=column, sticky='nsew')
+        return button
+    
+    def _create_text_box(self, row: int, column: int, rowspan: int, columnspan: int) -> tk.Entry:
+        text_box = tk.Entry(self)
+        text_box.grid(row=row, column=column, rowspan=rowspan, columnspan=columnspan, sticky='nsew')
+        return text_box
+        
+    def _create_combobox(self, row: int, column: int) -> ttk.Combobox:
+        cipher_mode = ['ECB', 'CBC']
+        self.style.configure("TCombobox", fieldbackground= "white", background= "white")
+        combobox = ttk.Combobox(self, state="readonly", textvariable=cipher_mode[0] , values=(cipher_mode))
+        combobox.set(cipher_mode[0])
+        combobox.grid(row=row, column=column, sticky='nsew')
+        return combobox
+
+
+
+    def select_file(self):
+        filetypes = (
+            ('text files', '*.txt'),
+            ('All files', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+
+        if not filename:
+            return
+
+        showinfo(
+            title='Selected File',
+            message=filename
+        )
+
+
+if __name__ == "__main__":
+    app = Gui("xd")
+    app.mainloop()
---
+++ app/key_gens.py
@@ -0,0 +1,5 @@
+def generate_rsa_keys():
+    raise NotImplementedError
+
+def send_rsa_key():
+    raise NotImplementedError
